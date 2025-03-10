d1f1e9ce0f701729532fe6628cb9d5de2676ee47
Aashutosh kumar pal <aashutoshkumarpal022@gmail.com>
2025-03-10 21:31:43 +0530

    [Aashutosh] Add . Section 1 UC1 Configure Database and Application Settings

---
+++ RepositoryLayer/Context/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<AddressBookEntry> AddressBookEntries { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AddressBookEntry>()
+                .HasOne(ab => ab.User)
+                .WithMany(u => u.AddressBookEntries)
+                .HasForeignKey(ab => ab.UserId);
+        }
+    }
+}
